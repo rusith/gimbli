10d4a4e8e9ee80a60e422b9d974f0d5cae29a13a
Shanaka Rusith <rusith@mail.com>
2020-01-01 09:44:37 +0530

    mockable fs module

--- src/__mocks__/fs.ts
+++ src/__mocks__/fs.ts
@@ -1,48 +1,69 @@
 "use strict";
 
+let readdirMock = (p, args, callback) => callback(null, [
+    {
+        isDirectory: () => false,
+        isFile: () => true,
+        name: "filename.txt",
+    },
+    {
+        isDirectory: () => false,
+        isFile: () => true,
+        name: "filename.png",
+    },
+    {
+        isDirectory: () => true,
+        isFile: () => false,
+        name: "templates",
+    },
+]);
+let readFileMock = (p, callback) => callback(null, "data");
+let writeFileMock = (p, content, callback) => callback(null, content);
+let mkdirMock = (p, options, callback) => callback(null, null);
+let existsMock = (p) => false;
+
+export function setMockFn(fn: any, mock: any) {
+    if (fn === readdir) {
+        readdirMock = mock;
+    } else if (fn === readFile) {
+        readFileMock = mock;
+    } else if (fn === writeFile) {
+        writeFileMock = mock;
+    } else if (fn === mkdir) {
+        mkdirMock = mock;
+    } else if (fn === exists) {
+        existsMock = mock;
+    }
+}
+
 // tslint:disable-next-line:no-var-requires
 const path = require("path");
 
 const fs = jest.genMockFromModule("fs") as any;
 
-function readdir(p, options, callback)  {
-    callback(null, [
-        {
-            isDirectory: () => false,
-            isFile: () => true,
-            name: "filename.txt",
-        },
-        {
-            isDirectory: () => false,
-            isFile: () => true,
-            name: "filename.png",
-        },
-        {
-            isDirectory: () => true,
-            isFile: () => false,
-            name: "templates",
-        },
-    ]);
+function readdir(p, opt, callback)  {
+    readdirMock(p, opt, callback);
 }
 
 function readFile(p, callback) {
-    callback(null, "data");
+    readFileMock(p, callback);
 }
 
 function writeFile(p, content, callback) {
-    callback(null, content);
+    writeFileMock(p, content, callback);
 }
 
 function mkdir(p, options, callback) {
-    callback(null, null);
+    mkdirMock(p, options, callback);
 }
 
 function exists(pa) {
-    return false;
+    existsMock(pa);
 }
 
 fs.readdir = readdir;
 fs.readFile = readFile;
 fs.writeFile = writeFile;
 fs.mkdir = mkdir;
+fs.setMockFn = setMockFn;
 module.exports = fs;
